108db0688eb96140f7e38144b559fba08b264936
Siam Sadman <sadman.bin.anwar.siam@gmail.com>
2025-06-01 21:09:22 +0600

    testing

--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h>Study Sidekick</h>
+      <br></br>
+      <Link href={`/app/profile`}>
+        <Button className="">Testing Button</Button>
+      </Link>
     </div>
   );
 }
---
+++ app/profile/page.js
@@ -0,0 +1,7 @@
+export default function profile() {
+  return (
+    <div>
+      <h>This is the profile page</h>
+    </div>
+  );
+}
