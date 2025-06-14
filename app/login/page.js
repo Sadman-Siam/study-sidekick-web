6e9026645a7a6ed5647b1ed050f9d5026bb99ef7
Siam Sadman <sadman.bin.anwar.siam@gmail.com>
2025-06-14 23:16:24 +0600

    setup auth

--- app/login/page.js
+++ app/login/page.js
@@ -1,3 +1,7 @@
+"use client";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -10,8 +14,37 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-
 export default function CardDemo() {
+  const googlelogin = async () => {
+    const firebaseConfig = {
+      apiKey: process.env.apiKey,
+      authDomain: process.env.authDomain,
+      projectId: process.env.projectId,
+      storageBucket: process.env.storageBucket,
+      messagingSenderId: process.env.messagingSenderId,
+      appId: process.env.appId,
+      measurementId: process.env.measurementId,
+    };
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    const provider = new GoogleAuthProvider(app);
+    const auth = getAuth(app);
+    auth.languageCode = "it";
+
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        const user = result.user;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
   return (
     <div className="flex justify-center items-center w-full pt-10">
       <Card className="w-1/3">
@@ -55,7 +88,7 @@ export default function CardDemo() {
           <Button type="submit" className="w-full">
             Login
           </Button>
-          <Button variant="outline" className="w-full">
+          <Button variant="outline" className="w-full" onClick={googlelogin}>
             Login with Google
           </Button>
         </CardFooter>
